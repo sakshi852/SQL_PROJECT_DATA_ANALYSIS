7379fcb64059dafb35ae97eff312f307ba699a4b
sakshi852 <sakshi9439@gmail.com>
2024-05-30 01:45:04 +0530

    push command

--- job_data_analysis/5 Optimal skills.sql
+++ job_data_analysis/5 Optimal skills.sql
@@ -1,34 +1,19 @@
-with skills_demand as (
-    select 
-        skills_dim.skill_id,
-        skills, 
-        count(skills_job_dim.job_id) as demand
-    FROM job_postings_fact
+/* What are top optimal skills*/
+
+select
+    skills_dim.skill_id,
+    skills_dim.skills, 
+    count(skills_job_dim.job_id) as demand,
+    avg(salary_year_avg) as avg_salary
+FROM job_postings_fact
     inner join skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
     inner join skills_dim on skills_job_dim.skill_id = skills_dim.skill_id 
     where 
-        job_title_short = 'Data Analyst' 
+        job_title_short = 'Data Analyst'
         and salary_year_avg is not null
-    group by skills_dim.skill_id
-),
-
-average_salary as (
-    select 
-        skills_dim.skill_id, 
-        skills,
-        avg(salary_year_avg) as avg_salary
-    FROM job_postings_fact
-    inner join skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
-    inner join skills_dim on skills_job_dim.skill_id = skills_dim.skill_id 
-    where 
-        job_title_short = 'Data Analyst' 
-        and salary_year_avg is not null
-    group by skills_dim.skill_id
-)
-
-select skills_demand.skill_id,
-    skills_demand.skills,
-    demand,
-    avg_salary
-from skills_demand
-inner join average_salary on skills_demand.skill_id=average_salary.skill_id
+group by skills_dim.skill_id
+HAVING count(skills_job_dim.job_id)>10
+order by 
+    avg_salary DESC,
+    demand DESC    
+limit 25;
